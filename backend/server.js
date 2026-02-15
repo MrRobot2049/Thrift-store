124a4b81cbd97e9f7912b8e4cbd5b0624edf23c1
Goutham Naroju <gouthamnaroju@Gouthams-MacBook-Air.local>
2026-02-15 12:33:08 +0530

    folders added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
-  res.send("Backend running...");
+  res.send("API is running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
