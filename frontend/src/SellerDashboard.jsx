24f7f97fa8a8db5ab73540a9dad444805a1fe98d
Ritvik29-creater <Kuthalritvik@gmail.com>
2026-03-13 10:49:17 +0530

    Checking

--- frontend/src/SellerDashboard.jsx
+++ frontend/src/SellerDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import "./sellerDashboard.css";
-
+//Checking 
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
 
